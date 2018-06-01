173f0526ae82208053d48cd67dd8b3977a7985df
Carlos Pontes <ccarlosdiordan@gmail.com>
2018-06-01 12:57:28 -0300

    Initial commit

---
+++ app/src/main/java/android/support/constraint/ConstraintLayout.java
@@ -0,0 +1,11 @@
+package android.support.constraint;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+class ConstraintLayout extends View {
+    public ConstraintLayout(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+}
